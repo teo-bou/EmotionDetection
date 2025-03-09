fafc4fb73a60ff035cc282b5955e2152cac7ca9b
Téo <teo@MacBookPro.lan>
2025-03-09 22:07:51 +0100

    Fixed overwrite image

--- App.py
+++ App.py
@@ -75,12 +75,14 @@ def main():
             if results.detections:
                 try:
                     detection = results.detections[0]
-                    mp_drawing.draw_detection(frame, detection)
                     xmin, ymin, width, height = int(detection.location_data.relative_bounding_box.xmin * frame.shape[1]), int(detection.location_data.relative_bounding_box.ymin * frame.shape[0]), int(detection.location_data.relative_bounding_box.width * frame.shape[1]), int(detection.location_data.relative_bounding_box.height * frame.shape[0])
                     face = frame[ymin:ymin+height, xmin:xmin+width]
                     face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
                     face = cv2.resize(face, (48, 48))
                     prediction = predict_CNN(face, model)
+                    mp_drawing.draw_detection(frame, detection)
+                    fps = cap.get(cv2.CAP_PROP_FPS)
+                    cv2.putText(frame, f'FPS: {fps:.2f}', (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
                     cv2.putText(frame, f'Emotion: {prediction}', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
                 except cv2.error as e:
                     print(f'Error {e}')
